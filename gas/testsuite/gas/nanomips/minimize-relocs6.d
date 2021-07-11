81fdba012b537359d664578ed3a8dd52fe49ae05
Faraz Shahbazker <faraz.shahbazker@mediatek.com>
2021-07-12 05:31:51 +0800

    CR MOLY00690538: Fix size accounting for lapc
    gas/
    	* config/tc-nanomips.c (record_insn_sops): Add new parameter
    	to indicate lapc instruction.
    	(append_insn): Record sops for lapc and regard it as a regular
    	relocated instruction instead of a PC-relative branch.
    	(relaxed_invariable_branch_p): Skip over branches that are
    	determined to be variable after the first pass.
    	* testsuite/gas/nanomips/minimize-relocs6.s
    	* testsuite/gas/nanomips/minimize-relocs6.d

--- gas/testsuite/gas/nanomips/minimize-relocs6.d
+++ gas/testsuite/gas/nanomips/minimize-relocs6.d
@@ -11,9 +11,21 @@ Disassembly of section \.text:
 00000000 <main>:
    0:	9008      	nop
 
-00000002 <\$L9>:
-   2:	aa07 0000 	bnec	a3,s0,2a <\$L12>
-			2: R_NANOMIPS_PC14_S1	\$L12
-   6:	aa06 0000 	bnec	a2,s0,2 <\$L9>
-			6: R_NANOMIPS_PC14_S1	\$L9
+00000002 <\$L1>:
+   2:	aa07 0000 	bnec	a3,s0,26 <\$L2>
+			2: R_NANOMIPS_PC14_S1	\$L2
+   6:	aa06 0000 	bnec	a2,s0,2 <\$L1>
+			6: R_NANOMIPS_PC14_S1	\$L1
+#...
+00000026 <\$L2>:
+  26:	aa07 0000 	bnec	a3,s0,52 <\$L3>
+			26: R_NANOMIPS_PC14_S1	\$L3
+  2a:	aa06 3ff9 	bnec	a2,s0,26 <\$L2>
+  2e:	8000 c000 	nop
+#...
+00000052 <\$L3>:
+  52:	aa07 0000 	bnec	a3,s0,84 <\$L4>
+			52: R_NANOMIPS_PC14_S1	\$L4
+  56:	aa06 0000 	bnec	a2,s0,52 <\$L3>
+			56: R_NANOMIPS_PC14_S1	\$L3
 #pass
