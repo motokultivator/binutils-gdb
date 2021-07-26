f1e0366f501cf4d9515d1668dfda6a56f45a6e42
faraz.shahbazker <faraz.shahbazker@mediatek.com>
2021-07-26 12:24:56 +0800

    Revert "CR MOLY00690538: Fix size accounting for lapc"
    This reverts commit 81fdba012b537359d664578ed3a8dd52fe49ae05.
    
    Reason for revert: Patch causes missing relocations on LAPC instructions referring to global/external symbols in some cases.
    
    Change-Id: Ib6d922ff174b4279752d94cab40403f6abdf583e

--- gas/testsuite/gas/nanomips/minimize-relocs6.s
+++ gas/testsuite/gas/nanomips/minimize-relocs6.s
@@ -1,61 +1,45 @@
 /* Bug-fix test-case:
-	MOLY00690538 - test reloc minimization at boundary conditions.  */
-
-	.linkrelax
-	.module pcrel
-	 .section	.rodata.str1.4,"aMS",@progbits,1
-	.align  2
+	Potential range of branch to $L12 extends from 32 to 40.
+	Typo in assembler code was overestimating the lower end
+	of the range by resetting relax_sink instead of aggregating
+	it at the final step.  The overestimation was outside the
+	relaxation range and the branch was assumed to be non-relaxable
+	to assembler.  Subsequent backward branch to $L9 which spans this
+	branch was then incorrectly assumed to be fixed which made its
+	target incorrect when the preceeding branch was relaxed by the
+	linker.
+*/
+        .linkrelax
+        .module pcrel
+         .section        .rodata.str1.4,"aMS",@progbits,1
+        .align  2
 $LC0:
-	.ascii  "0\000"
+        .ascii  "0\000"
 	.text
-	.align  1
-	.globl  main
-	.ent    main
-	.type   main, @function
+        .align  1
+        .globl  main
+        .ent    main
+        .type   main, @function
 main:
-	.set    noreorder
-	.set    nomacro
+        .set    noreorder
+        .set    nomacro
 	nop
-$L1:
-	/* Forward conditional branch may be relaxed from 32 to 16 bits.  */
-	bnec    $a3,$s0,$L2
+$L9:
+	/* Forward conditional branch which may be relaxed.  */
+        bnec    $a3,$s0,$L12
 	/* Backword conditional branch cannot be relaxed but offset is not
 	fixed due to preceeding variable branch. This reference must remain
 	relocable in object file.  */
-	bnec    $s0,$a2,$L1
-	.rept 5
-	nop32
-	.endr
-	lapc  $a0,$LC0
-	balc    __assert_func
-$L2:
-	/* Forward conditional branch that cannot be relaxed to 16-bit
-	   or 32-bit by virtue of its possible range at the lower end not
-	   fitting within a 5-bit offset.  */
-	bnec    $a3,$s0,$L3
-	/* Backword conditional branch cannot be relaxed and offset is
-	fixed due to preceeding invariable branch. This reference will not be
-	relocable in object file.  */
-	bnec    $s0,$a2,$L2
-	.rept 7
-	nop32
-	.endr
-	lapc  $a0,$LC0
-	balc    __assert_func
-$L3:
-	/* Check that the sink for lapc is correctly applied,  keeping this
-	branch potentially relaxable at the lower range (offset <= 30).  */
-	bnec    $a3,$s0,$L4
-	/* This reference must be relocatable in object file.  */
-	bnec    $s0,$a2,$L3
+        bnec    $s0,$a2,$L9
 	nop32
 	nop32
+	nop32
+	nop32
+        lapc.b  $a2,$LC0
+        lapc.b  $a0,$LC0
+        balc    __assert_func
+$L12:
 	nop
-	.rept 8
-	lapc  $a0,$LC0
-	.endr
-$L4:
-	nop
-	.set    macro
-	.set    reorder
-	.end    main
+        .set    macro
+        .set    reorder
+        .end    main
